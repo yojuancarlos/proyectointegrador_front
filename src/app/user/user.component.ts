fb12b428618fd64c4427eda14cb61cee1dd0f694
juancarlos saldarriaga urrea <56267375+yojuancarlos@users.noreply.github.com>
2024-12-04 16:07:12 -0500

    agregar el formulario de usuario
    ya se puede ver la pantalla principal desde el ususario

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,23 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import * as XLSX from 'xlsx';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
-  selector: 'app-user',
+  selector: 'app-admin',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AdminComponent implements OnInit {
+  sidebarOpen = true;
+
+  tableData: any[] = [];
+  tableHeaders: string[] = [];
+  userFiles: string[] = []; // Lista de archivos subidos
+
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
 
   }
 
-  onLogout(): void {
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  createProject(): void {
+    alert('¡Funcionalidad para crear un proyecto próximamente!');
+  }
+
+  openNewProjectPage(): void {
+    this.router.navigate(['/admin/create-project']); // Ruta correcta para la creación del proyecto
 
   }
+
+
+
+
+
 }
+
+
+
+
+
